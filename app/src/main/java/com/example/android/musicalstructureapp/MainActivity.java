f9fad6925f6d806277931d9d5166b8a5b892c6a3
PaolaMu <pmontielu@hotmail.com>
2018-06-18 15:27:35 -0500

    button

--- app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -20,17 +19,17 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.list);
         Database db = new Database();
         final ArrayList<Song> songs = db.getSongs();
-        SongAdapter adapter= new SongAdapter(this, R.layout.item_song, songs);
+        SongAdapter adapter = new SongAdapter(this, R.layout.item_song, songs);
 
-        ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = findViewById(R.id.list);
 
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-         Song clickSong = songs.get(position);
-                Intent songIntent = new Intent(MainActivity.this,NowPlayingActivity.class);
-                songIntent.putExtra("song",clickSong);
+                Song clickSong = songs.get(position);
+                Intent songIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                songIntent.putExtra("song", clickSong);
                 startActivity(songIntent);
 
             }
--- app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
@@ -3,20 +3,28 @@ package com.example.android.musicalstructureapp;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import com.squareup.picasso.Picasso;
 
+
+
+
 public class NowPlayingActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
+
 
         final Song song = getIntent().getParcelableExtra("song");
 
@@ -38,5 +46,19 @@ public class NowPlayingActivity extends AppCompatActivity {
             }
         });
 
+        final ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
